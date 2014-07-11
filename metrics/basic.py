3dfbb681f663e04797ccb5a42cb2442ea26487ab
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-07-11 05:40:14 +0100

    Style. Be more pythonic. Update unit-test.

--- metrics/basic.py
+++ metrics/basic.py
@@ -5,11 +5,10 @@ def residuals(parameters, independent, function, measured):
     res = measured - function(parameters, independent)
     return res
 
+
 def sum_squared_residuals(parameters, independent, function, measured):
-    ssr = 0
-    for res in residuals(parameters, independent, function, measured):
-        ssr += res**2
-    return ssr
+    return math.fsum(res**2 for res in residuals(parameters, independent, function, measured))
+
 
 def sum_absolute_value_residuals(values):
     return math.fsum([math.fabs(value) for value in values])
--- test/metrics/tests.py
+++ test/metrics/tests.py
@@ -10,14 +10,13 @@ class test_metrics(unittest.TestCase):
     def test_sum_absolute_value_residuals(self):
         values = [0, 1, 2, 3, 4, 5, -1, -2, -3, -4, -5]
         self.assertEqual(metrics.basic.sum_absolute_value_residuals(values), 30)
-        return
+
     
     def test_sum_squared_residuals(self):
-        values = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        values = [1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
         def linear(p, x):
             return x
-        self.assertEqual(0, metrics.basic.sum_squared_residuals([1.0], values, linear, numpy.ones(len(values))))
-        return
+        self.assertEqual(40, metrics.basic.sum_squared_residuals([1.0], values, linear, numpy.ones(len(values))))
 
 
 if __name__ == "__main__":
