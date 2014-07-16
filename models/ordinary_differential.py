edd0944a219b73a483b47470470b4e8f78261816
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-07-16 22:06:50 +0100

    Implement epo receptor model; unit-test function evaluation.

--- models/ordinary_differential.py
+++ models/ordinary_differential.py
@@ -1,4 +1,6 @@
 
+import numpy
+
 # TODO: change to gain and time constant and first order
 def linear(states, time, parameters, inputs):
     assert(len(states) == 1)
@@ -41,3 +43,40 @@ def linear_st(y, t, instance):
     assert(len(instance["inputs"]) == 1)
     instance["states"][0] = y
     return instance["parameters"][0] * instance["inputs"][0] - instance["states"][0]
+
+
+def epo_receptor(states, time, params, inputs):
+    d_dt = {
+        "Epo": 0.0,
+        "EpoR": 0.0,
+        "Epo_EpoR": 0.0,
+        "Epo_EpoR_i": 0.0,
+        "dEpo_i": 0.0,
+        "dEpi_e": 0.0,
+    }
+    v = {
+        "1": 0.0,
+        "2": 0.0,
+        "3": 0.0,
+        "4": 0.0,
+        "5": 0.0,
+        "6": 0.0,
+        "7": 0.0,
+        "8": 0.0,
+    }
+    v["1"] = params["k_on"]*states["Epo"]*states["EpoR"]
+    v["2"] = params["k_off"]*states["Epo_EpoR"]
+    v["3"] = params["k_t"]*inputs["B_max"]
+    v["4"] = params["k_t"]*states["EpoR"]
+    v["5"] = params["k_e"]*states["Epo_EpoR"]
+    v["6"] = params["k_ex"]*states["Epo_EpoR_i"]
+    v["7"] = params["k_di"]*states["Epo_EpoR_i"]
+    v["8"] = params["k_de"]*states["Epo_EpoR_i"] 
+    d_dt["Epo"] = -v["1"]+v["2"]+v["6"]
+    d_dt["EpoR"] = -v["1"]+v["2"]+v["3"]-v["4"]+v["6"]
+    d_dt["Epo_EpoR"] = v["1"]-v["2"]-v["5"]
+    d_dt["Epo_EpoR_i"] = v["5"]-v["6"]-v["7"]-v["8"]
+    d_dt["dEpo_i"] = v["7"]
+    d_dt["dEpo_e"] = v["8"]
+    return numpy.array([d_dt["Epo"], d_dt["EpoR"], d_dt["Epo_EpoR"], d_dt["Epo_EpoR_i"], d_dt["dEpo_i"], d_dt["dEpo_e"]])
+
--- test/models/ordinary_differential.py
+++ test/models/ordinary_differential.py
@@ -1,18 +1,46 @@
 
 import unittest
 
+import numpy
+
 import models.ordinary_differential
 
 class TestOrdinaryDifferentialModels(unittest.TestCase):
 
     def test_linear_st(self):
-        instance =dict(models.ordinary_differential.model_structure)
+        instance = dict(models.ordinary_differential.model_structure)
         instance["states"] = [1.0]
         instance["inputs"] = [1.0]
         instance["parameters"] = [1.0]
         instance["time"] = 0.0
         self.assertEquals(0.0, models.ordinary_differential.linear_st(instance["states"][0], instance["time"], instance))
 
+
+    def test_epo_receptor(self):
+        params = {
+            "k_on": 0.00010496,
+            "k_off": 0.0172135,
+            "k_t": 0.0329366,
+            "k_e": 0.0748267,
+            "k_ex": 0.00993805,
+            "k_di": 0.00317871,
+            "k_de": 0.0164042,
+        }
+        states = {
+            "Epo": 0.0,
+            "EpoR": 0.0,
+            "Epo_EpoR": 0.0,
+            "Epo_EpoR_i": 0.0,
+            "dEpo_i": 0.0,
+            "dEpo_e": 0.0,
+        }
+        inputs = {
+            "B_max": 0.0,
+        }
+        time = 0.0
+        actual = models.ordinary_differential.epo_receptor(states, time, params, inputs)
+        expected = numpy.zeros(6)
+        [self.assertEquals(act, exp) for act, exp in zip(actual, expected)]
 
 if __name__ == "__main__":
     unittest.main()
