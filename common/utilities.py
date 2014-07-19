d9706d6c0151f1fa537b34c2e959b25108835b0f
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-07-19 05:56:55 +0100

    Test. Extend utility to cope with multiple sets of points (not only a single set). Update unit-tests.

--- common/utilities.py
+++ common/utilities.py
@@ -2,12 +2,15 @@
 import numpy
 
 def sliceit(packed_vector):
-    points = []
-    for point in packed_vector:
-        points.append(point[0])
-    return points
+    collection_of_points = []
+    for points in packed_vector:
+        stack_of_points = []
+        for point in points:
+            stack_of_points.append(point)
+        collection_of_points.append(points)
+    return collection_of_points
 
 
 def sliceit_asarray(packed_vector):
-    points = sliceit(packed_vector)
-    return numpy.asarray(points)
+    stack_of_points = sliceit(packed_vector)
+    return numpy.asarray(stack_of_points)
--- test/common/utilities.py
+++ test/common/utilities.py
@@ -7,22 +7,24 @@ import numpy
 class TestUtilities(unittest.TestCase):
 
 
-    def test_sliceit(self):
+    def test_sliceit_1d(self):
         aslice = [1, 2, 3, 4, 5]
         whole = []
         whole.append(aslice)
         actual = common.utilities.sliceit(whole)
-        expected = numpy.array(1)
-        self.assertEquals(expected, actual)
+        expected = []
+        expected.append(aslice)
+        [self.assertEquals(exp, act) for exp, act in zip(expected[0], actual[0])]
 
     
-    def test_sliceit_asarray(self):
+    def test_sliceit_asarray_1d(self):
         aslice = [1, 2, 3, 4, 5]
         whole = []
         whole.append(aslice)
         actual = common.utilities.sliceit_asarray(whole)
-        expected = numpy.array(1)
-        self.assertEquals(expected, actual)
+        expected = []
+        expected.append(numpy.asarray(aslice))
+        [self.assertEquals(exp, act) for exp, act in zip(expected[0], actual[0])]
 
 
 if __name__ == "__main__":
