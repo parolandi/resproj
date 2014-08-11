fdd0f9be5a537cfe105f8175d1cb3507bf0fdb79
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-08-11 22:09:55 +0100

    Implement t-student approximate confidence interval test; unit-test.

--- metrics/statistical_tests.py
+++ metrics/statistical_tests.py
@@ -1,6 +1,9 @@
 
 import scipy.stats
 
+import common.exceptions
+
+
 # TODO: unencrypt these user messages
 user_messages = {
     "one_sided_chi_squared_test_values": "Sum of residuals value {}, chi-squared value: {}",
@@ -15,6 +18,8 @@ user_messages = {
     "two_sided_chi_squared_test_fail_lower_oneliner": "OOPS! Too small sum of residuals",
     "two_sided_chi_squared_test_fail_upper_oneliner": "OOPS! Too large sum of residuals",
     "two_sided_chi_squared_test_fail_description": "blah, blah",
+    "two_sided_t_student_test_pass_oneliner": "OK! Student-t value is less than reference t-value",
+    "two_sided_t_student_test_fail_oneliner": "OOPS! Student-t value is less than reference t-value",
     }
 
 # TODO: validation of user input
@@ -55,4 +60,26 @@ def calculate_two_sided_chi_squared_test_for_mean_sum_squared_residuals(res, dof
             print(user_messages["two_sided_chi_squared_test_fail_lower_oneliner"])
         if res >= chi_squared_value_upper:
             print(user_messages["two_sided_chi_squared_test_fail_upper_oneliner"])
+        return False
+
+
+# parameter_value, standard_error: such that t=(parameter_value-trueparameter_value)/standard_error
+# dof: degrees-of-freedom
+# significance: probability
+# the significance is two-sided
+# TODO: add message descriptions
+def calculate_two_sided_t_student_test_for_parameter_estimates(parameter_value, standard_error, dof, significance):
+    common.exceptions.assert_throw(standard_error > 0.0)
+    common.exceptions.assert_throw(parameter_value > 0.0)
+    common.exceptions.assert_throw(dof > 0)
+    common.exceptions.assert_throw(significance > 0.0 and significance < 1.0)
+    
+    q = (1-significance)/2
+    reference_t_stat = scipy.stats.t.isf(q, dof)
+    value_t_stat = parameter_value / standard_error
+    if value_t_stat > reference_t_stat:
+        print(user_messages["two_sided_t_student_test_pass_oneliner"])
+        return True
+    else:
+        print(user_messages["two_sided_t_student_test_fail_oneliner"])
         return False
--- test/metrics/statistical_tests.py
+++ test/metrics/statistical_tests.py
@@ -3,6 +3,7 @@ import unittest
 
 import metrics.statistical_tests
 
+
 class TestStatisticalTest(unittest.TestCase):
 
 
@@ -24,6 +25,16 @@ class TestStatisticalTest(unittest.TestCase):
         self.assertTrue(actual)
         actual = metrics.statistical_tests.calculate_two_sided_chi_squared_test_for_mean_sum_squared_residuals(124.343, 100, 0.90)
         self.assertFalse(actual)
+        
+        
+    def test_two_sided_t_student_test(self):
+        # @ dof=100
+        t_cutoff = 1.6602343261
+        t_offset = 0.01
+        actual = metrics.statistical_tests.calculate_two_sided_t_student_test_for_parameter_estimates(t_cutoff - t_offset, 1, 100, 0.90)
+        self.assertFalse(actual)
+        actual = metrics.statistical_tests.calculate_two_sided_t_student_test_for_parameter_estimates(t_cutoff + t_offset, 1, 100, 0.90)
+        self.assertTrue(actual)
 
 
 if __name__ == "__main__":
