8e2cbcb47448494d774d5ab913f9ddde67a4fe69
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-08-24 05:25:12 +0100

    Add calculation of vectorial sum of sequared residuals; one for each observable.

--- metrics/ordinary_differential.py
+++ metrics/ordinary_differential.py
@@ -42,6 +42,24 @@ def residuals_dof(dof, model, model_instance, problem_instance):
     return residuals_st(model, model_instance, problem_instance)
 
 
+
+def sums_squared_residuals(dof, model, model_instance, problem_instance):
+    if len(problem_instance["parameter_indices"]) > 0:
+        for ii in range(len(dof)):
+            index = problem_instance["parameter_indices"][ii]
+            model_instance["parameters"][index] = dof[ii]
+            problem_instance["parameters"][ii] = dof[ii]
+
+    sum_res = []
+    residuals = residuals_st(model, model_instance, problem_instance)
+    if len(problem_instance["output_indices"]) == 1:
+        sum_res.append(math.fsum(res**2 for res in residuals_st(model, model_instance, problem_instance)))
+        return sum_res
+    for ii in range(len(problem_instance["outputs"])):
+        sum_res.append(math.fsum(res**2 for res in residuals_st(model, model_instance, problem_instance)[ii]))
+    return sum_res
+
+
 # TODO: compute state-wise and experiment-wise
 # TODO: rename; remove _st
 def sum_squared_residuals_st(dof, model, model_instance, problem_instance):
