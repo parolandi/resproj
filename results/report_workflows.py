4e7e2905107392bd1dcd5cec318941fb92e231c6
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-08-30 07:22:42 +0100

    New. Report metrics of calibration and validation steps.

---
+++ results/report_workflows.py
@@ -0,0 +1,46 @@
+
+from __future__ import print_function
+
+
+workflow_data = {
+    "params": [],
+    "obj": [],
+    "obj_contribs": [],
+    "ssr": [],
+    "ssr_contribs": [],
+    }
+
+
+workflow_results = {
+    "full": dict(workflow_data),
+    "calibration": dict(workflow_data),
+    "validation": dict(workflow_data),
+    "calib+valid": dict(workflow_data), 
+    }
+
+
+def report_all(results):
+    print("Label", "Full", "Calib", "Valid", "Calib+Valid", "Sum")
+    
+    print("Params", \
+          results["full"]["params"], \
+          results["calibration"]["params"], \
+          results["validation"]["params"], \
+          results["calib+valid"]["params"], \
+          "[n/a]")
+    print("Obj", \
+          results["full"]["obj"][len(results["full"]["obj"])-1], \
+          results["calibration"]["obj"][len(results["calibration"]["obj"])-1], \
+          results["validation"]["obj"][len(results["validation"]["obj"])-1], \
+          results["calib+valid"]["obj"][len(results["calib+valid"]["obj"])-1], \
+          results["calibration"]["obj"][len(results["calibration"]["obj"])-1] + \
+          results["validation"]["obj"][len(results["validation"]["obj"])-1])
+    print("Obj contribs", \
+          results["full"]["obj_contribs"][len(results["full"]["obj_contribs"])-1], \
+          results["calibration"]["obj_contribs"][len(results["calibration"]["obj_contribs"])-1], \
+          results["validation"]["obj_contribs"][len(results["validation"]["obj_contribs"])-1], \
+          results["calib+valid"]["obj_contribs"][len(results["calib+valid"]["obj_contribs"])-1], \
+          [results["calibration"]["obj_contribs"][len(results["calibration"]["obj_contribs"])-1][0] +
+          results["validation"]["obj_contribs"][len(results["validation"]["obj_contribs"])-1][0], \
+          results["calibration"]["obj_contribs"][len(results["calibration"]["obj_contribs"])-1][1] +
+          results["validation"]["obj_contribs"][len(results["validation"]["obj_contribs"])-1][1]])
