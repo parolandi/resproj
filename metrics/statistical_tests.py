583a547622fb96874c5475647df030a052499106
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-08-31 19:08:48 +0100

    Add flag to control printing of user messages.

--- metrics/statistical_tests.py
+++ metrics/statistical_tests.py
@@ -5,7 +5,6 @@ import scipy.stats
 import common.exceptions
 import common.utilities
 
-
 # TODO: unencrypt these user messages
 user_messages = {
     "one_sided_chi_squared_test_info_oneliner": "Sum of residuals value: {}; chi-squared value: {}",
@@ -42,15 +41,21 @@ def calculate_degrees_of_freedom(measurements, parameters_to_be_estimated):
 # dof: degrees-of-freedom
 # significance: probability
 def calculate_one_sided_chi_squared_test_for_mean_sum_squared_residuals(res, dof, significance):
+    global print_user_messages
+    print_user_messages = False
+
     chi_squared_value = scipy.stats.chi2.isf(significance, dof)
-    print(user_messages["one_sided_chi_squared_test_info_oneliner"].format(res, chi_squared_value))
+    if print_user_messages:
+        print(user_messages["one_sided_chi_squared_test_info_oneliner"].format(res, chi_squared_value))
     if res < chi_squared_value:
-        print(user_messages["one_sided_chi_squared_test_pass_oneliner"])
-        print(user_messages["one_sided_chi_squared_test_pass_description"])
+        if print_user_messages:
+            print(user_messages["one_sided_chi_squared_test_pass_oneliner"])
+            print(user_messages["one_sided_chi_squared_test_pass_description"])
         return True
     else:
-        print(user_messages["one_sided_chi_squared_test_fail_oneliner"])
-        print(user_messages["one_sided_chi_squared_test_fail_description"])
+        if print_user_messages:
+            print(user_messages["one_sided_chi_squared_test_fail_oneliner"])
+            print(user_messages["one_sided_chi_squared_test_fail_description"])
         return False
 
 
@@ -59,20 +64,28 @@ def calculate_one_sided_chi_squared_test_for_mean_sum_squared_residuals(res, dof
 # significance: probability
 # the significance is joint two sided
 def calculate_two_sided_chi_squared_test_for_mean_sum_squared_residuals(res, dof, significance):
+    global print_user_messages
+    print_user_messages = False
+
     q = (1-significance)/2
     chi_squared_value_lower = scipy.stats.chi2.ppf(q, dof)
     chi_squared_value_upper = scipy.stats.chi2.isf(q, dof)
-    print(user_messages["two_sided_chi_squared_test_values"].format(res, chi_squared_value_lower, chi_squared_value_upper))
+    if print_user_messages:
+        print(user_messages["two_sided_chi_squared_test_values"].format(res, chi_squared_value_lower, chi_squared_value_upper))
     if res > chi_squared_value_lower and res < chi_squared_value_upper:
-        print(user_messages["two_sided_chi_squared_test_pass_oneliner"])
-        print(user_messages["two_sided_chi_squared_test_pass_description"])
+        if print_user_messages:
+            print(user_messages["two_sided_chi_squared_test_pass_oneliner"])
+            print(user_messages["two_sided_chi_squared_test_pass_description"])
         return True
     else:
-        print(user_messages["two_sided_chi_squared_test_fail_oneliner"])
+        if print_user_messages:
+            print(user_messages["two_sided_chi_squared_test_fail_oneliner"])
         if res <= chi_squared_value_lower:
-            print(user_messages["two_sided_chi_squared_test_fail_lower_oneliner"])
+            if print_user_messages:
+                print(user_messages["two_sided_chi_squared_test_fail_lower_oneliner"])
         if res >= chi_squared_value_upper:
-            print(user_messages["two_sided_chi_squared_test_fail_upper_oneliner"])
+            if print_user_messages:
+                print(user_messages["two_sided_chi_squared_test_fail_upper_oneliner"])
         return False
 
 
@@ -86,17 +99,23 @@ def calculate_two_sided_t_student_test_for_parameter_estimates(parameter_value, 
     common.exceptions.assert_throw(parameter_value > 0.0)
     common.exceptions.assert_throw(dof > 0)
     common.exceptions.assert_throw(significance > 0.0 and significance < 1.0)
+
+    global print_user_messages
+    print_user_messages = False
     
     q = (1-significance)/2
     reference_t_stat = scipy.stats.t.isf(q, dof)
     value_t_stat = parameter_value / standard_error
-    print(user_messages["two_sided_t_student_test_info_oneliner"].format(value_t_stat, reference_t_stat))
-    print(user_messages["two_sided_t_student_test_info_description"])
+    if print_user_messages:
+        print(user_messages["two_sided_t_student_test_info_oneliner"].format(value_t_stat, reference_t_stat))
+        print(user_messages["two_sided_t_student_test_info_description"])
     if value_t_stat > reference_t_stat:
-        print(user_messages["two_sided_t_student_test_pass_oneliner"])
-        print(user_messages["two_sided_t_student_test_pass_description"])
+        if print_user_messages:
+            print(user_messages["two_sided_t_student_test_pass_oneliner"])
+            print(user_messages["two_sided_t_student_test_pass_description"])
         return True
     else:
-        print(user_messages["two_sided_t_student_test_fail_oneliner"])
-        print(user_messages["two_sided_t_student_test_fail_description"])
+        if print_user_messages:
+            print(user_messages["two_sided_t_student_test_fail_oneliner"])
+            print(user_messages["two_sided_t_student_test_fail_description"])
         return False
