5b3c4860579b4b9ff889e94ed26fa3858f6a8b1d
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-09-17 21:08:43 +0100

    Add TODO comment.

--- solvers/solver_data.py
+++ solvers/solver_data.py
@@ -8,6 +8,7 @@ algorithm_structure = {
     "tolerance": None,
     }
 
+# TODO: clone as nonlinear programming methods too
 nonlinear_algebraic_methods = {
     "key-CG": 'CG',
     "key-hybr-suite-MINPACK-method-modified-Powell-ref-More-Garbow-Hillstrom-aff-ANL": 'hybr',
