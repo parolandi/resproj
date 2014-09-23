3cc3b937bfa9a558f3a21f70f125b3f5070e7bc3
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-09-23 06:28:24 +0100

    Style. Sort arguments alphabetically.

--- solvers/least_squares.py
+++ solvers/least_squares.py
@@ -5,19 +5,20 @@ import scipy.optimize
 import solvers.solver_data
 
 
+# TODO: rename; remove _st
 def solve_st(metric, model, model_instance, problem_instance, algorithm_structure):
     diag = {
         "disp": False,
         }
     return scipy.optimize.minimize( \
-        fun =     metric, \
-        x0 =      algorithm_structure["initial_guesses"], \
-        args =    (model, model_instance, problem_instance), \
-        method =  algorithm_structure["method"],
-        bounds =  problem_instance["bounds"],
-        callback = algorithm_structure["callback"],
-        options = diag,
-        tol = algorithm_structure["tolerance"],
+        args =     (model, model_instance, problem_instance), \
+        bounds =   problem_instance["bounds"], \
+        callback = algorithm_structure["callback"], \
+        fun =      metric, \
+        method =   algorithm_structure["method"], \
+        options =  diag, \
+        tol =      algorithm_structure["tolerance"], \
+        x0 =       algorithm_structure["initial_guesses"], \
         )
 
 
