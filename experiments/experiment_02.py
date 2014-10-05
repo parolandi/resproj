fca739d7c84c1c7911686e43fed24e7504502cf7
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-10-05 10:37:49 +0100

    Documentation. Describe experiments 02 and 03. Cleanup.

--- experiments/experiment_02.py
+++ experiments/experiment_02.py
@@ -5,17 +5,21 @@ import data.data_splicing
 import experiments.experiment
 
 
-NM_method = "key-Nelder-Mead"
-CG_method = "key-CG"
-
-
+'''
+Examine the effect of changing the number of data points between
+20, 30 and 40 points with the same data splicing pattern 111000
+using CG
+'''
 class TestExperiment02(unittest.TestCase):
 
+
+    CG_method = "key-CG"
+    
 
     def test_do_experiment_at_conditions_111000_and_20_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_111000
-        config["algorithm_setting"] = CG_method
+        config["algorithm_setting"] = self.CG_method
         config["number_of_intervals"] = 20
         _ = None
         experiments.experiment.do_experiment(self, config, _)
@@ -24,7 +28,7 @@ class TestExperiment02(unittest.TestCase):
     def test_do_experiment_at_conditions_111000_and_30_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_111000
-        config["algorithm_setting"] = CG_method
+        config["algorithm_setting"] = self.CG_method
         config["number_of_intervals"] = 30
         _ = None
         experiments.experiment.do_experiment(self, config, _)
@@ -33,7 +37,7 @@ class TestExperiment02(unittest.TestCase):
     def test_do_experiment_at_conditions_111000_and_40_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_111000
-        config["algorithm_setting"] = CG_method
+        config["algorithm_setting"] = self.CG_method
         config["number_of_intervals"] = 40
         _ = None
         experiments.experiment.do_experiment(self, config, _)
--- experiments/experiment_03.py
+++ experiments/experiment_03.py
@@ -6,18 +6,23 @@ import data.data_splicing
 import experiments.experiment
 
 
-number_of_intervals = 30
-CG_method = "key-CG"
-
-
+'''
+Examine the effect of changing the number of data points between
+20, 30 and 40 points with the same data splicing pattern 111000
+using CG
+'''
 class TestExperiment03(unittest.TestCase):
+
+
+    number_of_intervals = 30
+    CG_method = "key-CG"
 
 
     def test_do_experiment_at_conditions_110110_and_30_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_110110
-        config["algorithm_setting"] = "key-CG"
-        config["number_of_intervals"] = 30
+        config["algorithm_setting"] = self.CG_method
+        config["number_of_intervals"] = self.number_of_intervals
         _ = None
         experiments.experiment.do_experiment(self, config, _)
 
@@ -25,8 +30,8 @@ class TestExperiment03(unittest.TestCase):
     def test_do_experiment_at_conditions_101101_and_30_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_101101
-        config["algorithm_setting"] = "key-CG"
-        config["number_of_intervals"] = 30
+        config["algorithm_setting"] = self.CG_method
+        config["number_of_intervals"] = self.number_of_intervals
         _ = None
         experiments.experiment.do_experiment(self, config, _)
 
@@ -34,8 +39,8 @@ class TestExperiment03(unittest.TestCase):
     def test_do_experiment_at_conditions_011011_and_30_points_with_CG(self):
         config = dict(experiments.experiment.experiment_setup)
         config["data_splicing"] = data.data_splicing.splice_data_with_pattern_011011
-        config["algorithm_setting"] = "key-CG"
-        config["number_of_intervals"] = 30
+        config["algorithm_setting"] = self.CG_method
+        config["number_of_intervals"] = self.number_of_intervals
         _ = None
         experiments.experiment.do_experiment(self, config, _)
 
