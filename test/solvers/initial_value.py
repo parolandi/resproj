cad3750dd642e8b96ffed451cfedfc071f423d0e
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-10-10 06:47:36 +0100

    Test. Add 2x2 system; unit-test.

--- test/solvers/initial_value.py
+++ test/solvers/initial_value.py
@@ -1,9 +1,11 @@
 
 import unittest
+import solvers.initial_value
+
+import copy
 import numpy
 
 import common.utilities
-import solvers.initial_value
 import models.model_data
 
 
@@ -12,6 +14,15 @@ def linear_mock(x, t, p, u):
     return p[0] * u[0]
 
 
+def linear_2p2s_mock(x, t, p, u):
+    assert(len(x) == 2)
+    assert(len(p) == 2)
+    assert(len(u) == 2)
+    dx_dt = numpy.multiply(p, u)
+    return dx_dt
+
+
+# TODO: test with subset of "dofs"
 class TestInitialValueSolvers(unittest.TestCase):
 
     
@@ -30,6 +41,21 @@ class TestInitialValueSolvers(unittest.TestCase):
         return model_instance, problem_instance
 
 
+    def do_setup_2p2s(self):
+        problem_instance = dict(models.model_data.problem_structure)
+        problem_instance["initial_conditions"] = [0.0, 1.0]
+        problem_instance["parameters"] = [1.0, 0.5]
+        problem_instance["inputs"] = [1.0, 2.0]
+        
+        model_instance = dict(models.model_data.model_structure)
+        model_instance["parameters"] = copy.deepcopy(problem_instance["parameters"])
+        model_instance["inputs"] = copy.deepcopy(problem_instance["inputs"])
+        model_instance["states"] = copy.deepcopy(problem_instance["initial_conditions"])
+        model_instance["time"] = 0.0
+
+        return model_instance, problem_instance
+
+    
     # returns 10 points from 0.0:0.9 inclusive
     def do_setup_include_initial(self):
         model_instance, problem_instance = self.do_setup()
@@ -82,6 +108,41 @@ class TestInitialValueSolvers(unittest.TestCase):
         actual = solvers.initial_value.compute_timecourse_trajectories(linear_mock, model_instance, problem_instance)
         expected = [[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]]
         [self.assertAlmostEqual(exp, act, 8) for exp, act in zip(expected[0], actual[0])]
+
+
+    def test_linear_2p2s_solve_lsoda_st_include_initial(self):
+        model_instance, problem_instance = self.do_setup_2p2s()
+        problem_instance["time"] = numpy.linspace(0.0, 1.0, 10, endpoint=False)
+        
+        # avoid false negatives
+        model_instance["states"] = numpy.multiply(problem_instance["initial_conditions"], 1.1)
+        problem_instance["parameters"] = numpy.multiply(problem_instance["parameters"], 1.1)
+        problem_instance["inputs"] = numpy.multiply(problem_instance["inputs"], 1.1)
+                
+        result, _ = solvers.initial_value.solve_lsoda_st(linear_2p2s_mock, model_instance, problem_instance)
+        actual = common.utilities.sliceit_astrajectory(result)
+        expected = [[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+                    [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]]
+
+        [self.assertAlmostEqual(exp, act, 8) for exp, act in zip(expected[0], actual[0])]
+        [self.assertAlmostEqual(exp, act, 8) for exp, act in zip(expected[1], actual[1])]
+
+
+    def test_linear_2p2s_compute_trajectory_st_include_initial(self):
+        model_instance, problem_instance = self.do_setup_2p2s()
+        problem_instance["time"] = numpy.linspace(0.0, 1.0, 10, endpoint=False)
+        
+        # avoid false negatives
+        model_instance["states"] = numpy.multiply(problem_instance["initial_conditions"], 1.1)
+        problem_instance["parameters"] = numpy.multiply(problem_instance["parameters"], 1.1)
+        problem_instance["inputs"] = numpy.multiply(problem_instance["inputs"], 1.1)
+                
+        actual = solvers.initial_value.compute_timecourse_trajectories(linear_2p2s_mock, model_instance, problem_instance)
+        expected = [[0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
+                    [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9]]
+        
+        [self.assertAlmostEqual(exp, act, 8) for exp, act in zip(expected[0], actual[0])]
+        [self.assertAlmostEqual(exp, act, 8) for exp, act in zip(expected[1], actual[1])]
 
 
 if __name__ == "__main__":
