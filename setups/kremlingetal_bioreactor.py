6367176951699f7d7dfd401b3fd1445aca4ac2ac
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-11-22 21:07:41 +0000

    New. Add sensitivity setup for kremlingetal.

--- setups/kremlingetal_bioreactor.py
+++ setups/kremlingetal_bioreactor.py
@@ -12,6 +12,10 @@ import setups.setup_data
 import solvers.least_squares
 
 
+def get_parameters_to_be_estimated():
+    return [0, 3, 8, 9]
+
+
 def do_model_setup(model_key):
     p = numpy.ones(len(mkb.pmap))
     for par in mkb.pmap.items():
@@ -77,13 +81,45 @@ def do_problem_setup(model_data, data_instance):
     problem_data["inputs"] = copy.deepcopy(model_data["inputs"])
 
     problem_data["performance_measure"] = mod.sum_squared_residuals_st
-    problem_data["parameter_indices"] = [0, 3, 5, 8, 9]
+    problem_data["parameter_indices"] = get_parameters_to_be_estimated()
     problem_data["parameters"] = numpy.zeros(len(problem_data["parameter_indices"]))
     
     for ii in range(len(problem_data["parameter_indices"])):
         problem_data["parameters"][ii] = copy.deepcopy(model_data["parameters"][problem_data["parameter_indices"][ii]])
     
     problem_data["bounds"] = [(0,None), (0,None), (0,None), (0,None), (0,None)]
+    
+    problem_data["output_indices"] = [1, 2, 3, 4, 5]
+    problem_data["outputs"] = data_instance["observables"]
+    assert(len(["output_indices"]) == len(["outputs"]))
+
+    return problem_data
+
+
+def do_sensitivity_model_setup():
+    model = do_model_setup_model_B()
+    x = model["states"]
+    dim_dv = 4
+    model["states"] = numpy.hstack((x, numpy.zeros(len(mkb.xvec) * dim_dv)))
+    model["model"] = mkb.evaluate_system_and_sensitivities
+    return model
+
+
+def do_sensitivity_problem_setup(model_data, data_instance):
+    assert(model_data is not None)
+    assert(data_instance is not None)
+    
+    problem_data = dict(models.model_data.problem_structure)
+    problem_data["initial_conditions"] = copy.deepcopy(model_data["states"])
+    problem_data["time"] = data_instance["time"]
+    problem_data["parameters"] = copy.deepcopy(model_data["parameters"])
+    problem_data["inputs"] = copy.deepcopy(model_data["inputs"])
+
+    problem_data["parameter_indices"] = get_parameters_to_be_estimated()
+    problem_data["parameters"] = numpy.zeros(len(problem_data["parameter_indices"]))
+    
+    for ii in range(len(problem_data["parameter_indices"])):
+        problem_data["parameters"][ii] = copy.deepcopy(model_data["parameters"][problem_data["parameter_indices"][ii]])
     
     problem_data["output_indices"] = [1, 2, 3, 4, 5]
     problem_data["outputs"] = data_instance["observables"]
@@ -105,7 +141,7 @@ def do_algorithm_setup(instrumentation_data):
     p = numpy.ones(len(mkb.pmap))
     for par in mkb.pmap.items():
         p[par[1]] = mkb.pvec[par[0]]
-    pi = [0, 3, 5, 8, 9]
+    pi = get_parameters_to_be_estimated()
     initial_guesses = []
     for ii in range(len(pi)):
         initial_guesses.append(copy.deepcopy(p[pi[ii]]))
