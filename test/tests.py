31f92207efcfc59f86551123de1b32292bdeb37b
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-11-22 20:45:38 +0000

    Update. Add missing unit-tests!

--- test/tests.py
+++ test/tests.py
@@ -2,20 +2,28 @@
 import unittest
 
 # TODO:I don't know why import does not work directly
-#import test.metrics.ordinary_differential
 from test.common.utilities import *
-from test.metrics.algebraic import *
+from test.data.data_splicing import *
+from test.engine.estimation_matrices import *
+from test.engine.statistical_inference import *
 from test.metrics.algebraic_legacy import *
+from test.metrics.algebraic import *
 from test.metrics.confidence_measures import *
 from test.metrics.ordinary_differential import *
 from test.metrics.statistical_tests import *
+from test.models.algebraic import *
+from test.models.model_data_utils import *
 from test.models.ordinary_differential import *
-from test.solvers.initial_value import *
+#from test.results.plot_tiles import *
 from test.solvers.initial_value_legacy import *
-from test.solvers.nonlinear_algebraic import *
-from test.solvers.least_squares import *
+from test.solvers.initial_value import *
 from test.solvers.least_squares_algebraic import *
 from test.solvers.least_squares_legacy import *
+from test.solvers.least_squares import *
+from test.solvers.monte_carlo_multiple_least_squares import *
+from test.solvers.nonlinear_algebraic import *
+from test.solvers.solver_data import *
+
 
 if __name__ == '__main__':
     list_all = unittest.TestLoader().loadTestsFromTestCase()
