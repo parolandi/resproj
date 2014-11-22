94310c914c43e13d0a61113bd67c05a9141ede3a
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-11-22 20:40:51 +0000

    Bugfix. Prepare sensitivity matrix; uni-test.

--- engine/estimation_matrices.py
+++ engine/estimation_matrices.py
@@ -2,17 +2,26 @@
 import numpy
 
 
-def prepare_sensitivity_matrix(no_params, no_timepoints, sensitivity_trajectories):
-    assert(no_timepoints == len(sensitivity_trajectories[0]))
+#sensitivity_trajectories: [df1/dpT, df2/dpT, ..., dfn/dpT]T (n*p x t)
+'''
+Transform the list of sensitivity trajectories into a matrix of sensitivities
+return    numpy.matrix
+'''
+def prepare_sensitivity_matrix(no_states, no_params, no_timepoints, sensitivity_trajectories):
+    # TODO: general case with/without initial point
+    #assert(no_timepoints == len(sensitivity_trajectories[0]))
     
-    flat = sensitivity_trajectories.flatten()
-    no_meas = no_timepoints * no_params
-    sensitivity_matrix = numpy.asmatrix(flat.reshape((no_params, no_meas)).transpose())
+    matrix = []
+    for param in range(no_params):
+        param_sens_mask = [ii for ii in range(param, no_params*no_states, no_params)]
+        param_sens =  numpy.ndarray.flatten(sensitivity_trajectories[param_sens_mask])
+        matrix.append(param_sens)
+    sensitivity_matrix = numpy.transpose(numpy.asmatrix(matrix))
     return sensitivity_matrix
-    
 
-def compute_information_matrix(no_params, no_timepoints, sensitivity_trajectories):
-    sensitivity_matrix = prepare_sensitivity_matrix(no_params, no_timepoints, sensitivity_trajectories)
+
+def compute_information_matrix(no_states, no_params, no_timepoints, sensitivity_trajectories):
+    sensitivity_matrix = prepare_sensitivity_matrix(no_states, no_params, no_timepoints, sensitivity_trajectories)
     information_matrix = sensitivity_matrix.transpose().dot(sensitivity_matrix)
     return information_matrix
     
@@ -24,9 +33,13 @@ def calculate_information_matrix(sensitivity_matrix):
 
 
 # TODO: deal with singular case
-def compute_covariance_matrix(no_params, no_timepoints, sensitivity_trajectories):
-    information_matrix = compute_information_matrix(no_params, no_timepoints, sensitivity_trajectories)
-    covariance_matrix = numpy.linalg.inv(information_matrix)
+def compute_covariance_matrix(dim_states, no_params, no_timepoints, sensitivity_trajectories):
+    information_matrix = compute_information_matrix(dim_states, no_params, no_timepoints, sensitivity_trajectories)
+    try:
+        covariance_matrix = numpy.linalg.inv(information_matrix)
+    except:
+        print(information_matrix)
+        raise
     return covariance_matrix
 
     
