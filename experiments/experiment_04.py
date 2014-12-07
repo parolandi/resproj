ee93a71af076fb5b7afd1a3a19b097ea2a5a46f5
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-12-07 15:01:20 +0000

    Test; regression-test.

--- experiments/experiment_04.py
+++ experiments/experiment_04.py
@@ -3,6 +3,7 @@ import unittest
 import setups.kremlingetal_bioreactor as skb
 
 import copy
+import numpy
 import time
 
 import metrics.ordinary_differential as mo
@@ -142,6 +143,10 @@ class TestExperiment04(unittest.TestCase):
         actual = result["global"]["objective_function"]
         expected = 0.021174194991343515
         self.assertAlmostEquals(actual, expected, 12)
+        actual = result["global"]["decision_variables"]
+        expected = numpy.array([7.008707556656744e-05, 6969965.66008254, 0.007062287109842938, 0.13671901210795717])
+        deltas = numpy.array([0.000000000000001e-05, 0000000.00000001, 0.000000000000000001, 0.00000000000000001])
+        [self.assertAlmostEquals(act, exp, delta=diff) for act, exp, diff in zip(actual, expected, deltas)]
 
 
 if __name__ == "__main__":
