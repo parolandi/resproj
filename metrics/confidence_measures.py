dbb3be024c8387918e6b25e5bf758c14443b6eef
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-12-10 06:33:07 +0000

    Documentation.

--- metrics/confidence_measures.py
+++ metrics/confidence_measures.py
@@ -4,6 +4,9 @@ import numpy.linalg
 # returns an array
 # TODO: it should probably return a matrix, given the function's name
 def compute_covariance_matrix(sensitivities, covariance_observation_errors):
+    """
+    Do not use! Legacy code!
+    """
     cov = 0
     if len(covariance_observation_errors.shape) == 0:
         cov = numpy.dot(numpy.transpose(sensitivities), sensitivities) / covariance_observation_errors
