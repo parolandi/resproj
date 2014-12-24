0e34f06ca9d8be51c853c85ecf2afd68c04b9c62
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2014-12-24 06:22:36 +0000

    Extend baseline tests.

--- workflows/experiments.py
+++ workflows/experiments.py
@@ -1,8 +1,11 @@
 
-import models.model_data as mmd
-import workflows.protocols as wpr
-
 import common.diagnostics as cd
+import models.model_data as mmd
+import setups.setup_data_utils as ssdu
+import workflows.protocols as wpr
+import workflows.workflow_data_utils as wwdu
+
+import copy
 
 
 baseline = {
@@ -12,6 +15,11 @@ baseline = {
     }
 
 
+calib_valid_baseline = {
+    "calib": copy.deepcopy(baseline),
+    "valid": copy.deepcopy(baseline),
+    }
+
 # self.do_experiment_setup
 def test_baseline_calibration(setup, baseline, unittester):
     config = setup()
@@ -19,6 +27,8 @@ def test_baseline_calibration(setup, baseline, unittester):
     # output
     if True:
         cd.print_decision_variables_and_objective_function(calibrated)
+        post_proc = wpr.do_basic_workflow_at_solution_point(config, calibrated)
+        wwdu.print_system_based_point_results(post_proc)
     # test
     actual = calibrated["objective_function"]
     expected = baseline["point"]["objective_function"]
@@ -28,4 +38,39 @@ def test_baseline_calibration(setup, baseline, unittester):
     expected = baseline["point"]["decision_variables"]
     deltas = baseline["dv_deltas"]
     [unittester.assertAlmostEquals(act, exp, delta=diff) for act, exp, diff in zip(actual, expected, deltas)]
+    return calibrated
+
+
+# self.do_experiment_setup
+def test_baseline_validation(setup, baseline, unittester, point):
+    config = setup()
+    ssdu.set_next_protocol_step(config)
+    validated = wpr.do_validation_and_compute_performance_measure_at_solution_point(config, point)
+    # output
+    if True:
+        cd.print_decision_variables_and_objective_function(validated)
+        post_proc = wpr.do_basic_workflow_at_solution_point(config, validated)
+        wwdu.print_system_based_point_results(post_proc)
+    # test
+    actual = validated["objective_function"]
+    expected = baseline["point"]["objective_function"]
+    unittester.assertAlmostEquals(actual, expected, delta=baseline["of_delta"])
+    # test
+    actual = validated["decision_variables"]
+    expected = baseline["point"]["decision_variables"]
+    deltas = baseline["dv_deltas"]
+    [unittester.assertAlmostEquals(act, exp, delta=diff) for act, exp, diff in zip(actual, expected, deltas)]
+    return validated
+
+
+def test_baseline_calibration_and_validation(setup, baseline, unittester):
+    # there is no good reason why this should be different
+    refpoint = baseline["calib"]
+    basepoint = baseline["valid"]
+    basepoint["point"]["decision_variables"] = refpoint["point"]["decision_variables"]
+    basepoint["of_delta"] = refpoint["of_delta"]
+    basepoint["dv_deltas"] = refpoint["dv_deltas"]
+
+    calibrated = test_baseline_calibration(setup, baseline["calib"], unittester)
+    test_baseline_validation(setup, baseline["valid"], unittester, calibrated)
     return calibrated
