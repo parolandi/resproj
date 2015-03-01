Bugfix. Confidence interval calculation.

engine/statistical_inference.py
@@ -1,17 +1,18 @@
 
-import scipy.stats
+import math
 import numpy
+import scipy.stats
 
 
-'''
-Compute the confidence ellipsoid for the following assumptions:
-TODO:
-no_params: number of parameters estimated
-no_meas: number of measurement points
-est_stdev: estimated standard deviation (e.g., R/(n-p))
-significance: confidence level (e.g., 0.95 for 95%)
-'''
 def compute_confidence_ellipsoid_radius(no_params, no_meas, est_stdev, significance):
+    """
+    Compute the confidence ellipsoid for the following assumptions:
+    TODO:
+    no_params: number of parameters estimated
+    no_meas: number of measurement points
+    est_stdev: estimated standard deviation (e.g., R/(n-p))
+    significance: confidence level (e.g., 0.95 for 95%)
+    """
     assert(no_params > 0)
     assert(no_meas > 0)
     assert(no_meas > no_params)
@@ -26,21 +27,31 @@ def compute_confidence_ellipsoid_radius(no_params, no_meas, est_stdev, significa
     if user_messages:
         print("est-stdev: ", est_stdev)
         print("f-value: ", f_value)
+        print("radius:", radius)
 
     return radius
 
+
 # TODO: documentation
 def compute_measurements_standard_deviation(ssr, no_params, no_meas):
-    return ssr / (no_meas - no_params)
+    assert (no_meas > no_params)
+    
+    est_stdev = ssr / (no_meas - no_params)
+    user_messages = False
+    if user_messages:
+        print("ssr", ssr)
+        print("est-stdev", est_stdev)
+    
+    return est_stdev
 
 
 # TODO: documentation
-def compute_confidence_intervals(covariance_matrix, ellipsoid_radius):
-    assert(isinstance(ellipsoid_radius, float))
+def compute_confidence_intervals(covariance_matrix, t_value):
+    assert(t_value > 0)
     assert(len(covariance_matrix) > 0)
     
     no_params = len(covariance_matrix)
     confidence_intervals = numpy.zeros(no_params)
     for ii in range(no_params):
-        confidence_intervals[ii] = ellipsoid_radius * covariance_matrix[ii, ii]
+        confidence_intervals[ii] = t_value * math.sqrt(covariance_matrix[ii, ii])
     return confidence_intervals

metrics/statistical_tests.py
@@ -39,7 +39,10 @@ def calculate_one_sided_chi_squared_value(significance, dof):
     return scipy.stats.chi2.isf(significance, dof)
 
 
-# TODO: validation of user input
+def calculate_two_sided_t_student_value(significance, no_meas, no_params):
+    q = (1-significance)/2
+    return scipy.stats.t.isf(q, no_meas-no_params)
+
 
 # the significance is one sided, naturally
 # res: residual, maximum likelihood