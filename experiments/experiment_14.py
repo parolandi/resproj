8b5c7125f4f181a1a234d3e9f7460e4d4aaafa4c
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-04-08 07:19:24 +0100

    Test. Add regression.

--- experiments/experiment_14.py
+++ experiments/experiment_14.py
@@ -23,6 +23,7 @@ class TestExperiment14(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestExperiment14, self).__init__(*args, **kwargs)
         self.do_plotting = False
+        self.do_reduced_mc_sample = True
         logging.basicConfig(filename=codi.get_name_logging_file(),level=codi.get_logging_level())
 
     
@@ -88,15 +89,16 @@ class TestExperiment14(unittest.TestCase):
         best_point = self.do_regression(config)
         
         # setup lin conf reg
-        _ = ecr.compute_linearised_confidence_intervals(config, best_point)
-        #expected = baseline["intervals"]
-        #[self.assertAlmostEquals(act, exp, 8) for act, exp in zip(numpy.asarray(intervals).flatten(), expected.flatten())]
+        intervals = ecr.compute_linearised_confidence_intervals(config, best_point)
+        expected = baseline["intervals"]
+        [self.assertAlmostEquals(act, exp, 8) for act, exp in zip(numpy.asarray(intervals).flatten(), numpy.asarray(expected).flatten())]
         ellipsoid = ecr.compute_linearised_confidence_region_ellipsoid(config, best_point)
-        #expected = baseline["ellipsoid"]
-        #[self.assertAlmostEquals(act, exp, 8) for act, exp in zip( \
-        #    numpy.asarray(ellipsoid).flatten(), numpy.asarray(expected).flatten())]
+        expected = baseline["ellipsoid"]
+        diff = baseline["delta"]
+        [self.assertAlmostEquals(act, exp, delta=eps) for act, exp, eps in zip( \
+            numpy.asarray(ellipsoid).flatten(), numpy.asarray(expected).flatten(), numpy.asarray(diff).flatten())]
         
-        if True:
+        if self.do_plotting:
             replco.plot_combinatorial_ellipsoid_projections(best_point['decision_variables'], ellipsoid)        
 
     
@@ -107,6 +109,8 @@ class TestExperiment14(unittest.TestCase):
         model, problem, algorithm_rf = self.get_model_problem_algorithm(config())
         algorithm_mc = dict(mcmiv.montecarlo_multiple_simulation_params)
         algorithm_mc["number_of_trials"] = 80000
+        if self.do_reduced_mc_sample:
+            algorithm_mc["number_of_trials"] = 100
 
         if True:
             self.do_appy_bounds(best_point["decision_variables"], problem)
@@ -122,7 +126,7 @@ class TestExperiment14(unittest.TestCase):
         logging.info(wall_time)
         logging.info(algorithm_mc["number_of_trials"])
         logging.info(number_of_points)
-        #self.assertEquals(number_of_points, baseline["number_of_points"])
+        self.assertEquals(number_of_points, baseline["number_of_points"])
         
         # plot nonlin conf reg
         if self.do_plotting:
@@ -131,29 +135,72 @@ class TestExperiment14(unittest.TestCase):
 
     def test_ncr(self):
         baseline = {}
-        baseline["number_of_points"] = 7834
+        baseline["number_of_points"] = 80
+        baseline["intervals"] = [ \
+            [1.8560953554168591e-05, 0.00028858598850377325], \
+            [5999621.511495593, 311185069.71460104], \
+            [0.00063242352072096094, 0.040354449881673735], \
+            [0.0062924327562778317, 62.924327562777854]]
         baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "NCR benchmark model"
-        self.do_test_compute_nonlinear_confidence_region_points(self.do_experiment_setup, None)
-        self.assertFalse(True)
+        baseline["plotdata"]["window_title"] = "NCR benchmark model (95%)"
+        self.do_test_compute_nonlinear_confidence_region_points(self.do_experiment_setup, baseline)
 
      
+    def test_ncr_low_confidence(self):
+        baseline = {}
+        baseline["number_of_points"] = 81
+        baseline["intervals"] = [ \
+            [1.9918149990245181e-05, 0.00027202677048785735], \
+            [5999903.1790274335, 6000588.7692325944], \
+            [0.00070910862594929702, 0.037986101795423888], \
+            [0.0062924327298415237, 62.924327562777854]]
+        baseline["plotdata"] = dict(replda.plot_data)
+        baseline["plotdata"]["window_title"] = "NCR benchmark model (25%)"
+        self.do_test_compute_nonlinear_confidence_region_points(self.do_experiment_setup_with_low_confidence, baseline)
+
+    
     def test_lcr(self):
         baseline = {}
-        baseline["number_of_points"] = -1
+        baseline["intervals"] = [ \
+            [4.9423053215497172e-05, 0.00026694444088288676], \
+            [-113597360.94805983, 125597359.64413485], \
+            [-0.26987827573815093, 0.33646951374979961], \
+            [-513.87991160491765, 515.13839815617325]]
+        baseline["ellipsoid"] = [ \
+            [  6.12718097e-06, -1.09502057e+05,  8.69533106e-04, -2.02686196e+00], \
+            [ -1.09502057e+05,  7.40900618e+18,  5.94586975e+08, -8.05367692e+12], \
+            [  8.69533106e-04,  5.94586975e+08,  4.76102404e+01, -6.33519915e+04], \
+            [ -2.02686196e+00, -8.05367692e+12, -6.33519915e+04,  1.37120687e+08]]
+        baseline["delta"] = [ \
+            [  0.00000001e-06,  0.00000001e+05,  0.00000001e-04,  0.00000001e+00], \
+            [  0.00000001e+05,  0.00000001e+18,  0.00000001e+08,  0.00000001e+12], \
+            [  0.00000001e-04,  0.00000001e+08,  0.00000001e+01,  0.00000001e+04], \
+            [  0.00000001e+00,  0.00000001e+12,  0.00000001e+04,  0.00000001e+08]]
         baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "LCR benchmark model"
-        self.do_test_compute_linearised_confidence_region(self.do_experiment_setup(), None)
-        self.assertFalse(True)
+        baseline["plotdata"]["window_title"] = "LCR benchmark model (95%)"
+        self.do_test_compute_linearised_confidence_region(self.do_experiment_setup(), baseline)
 
 
     def test_lcr_low_confidence(self):
         baseline = {}
-        baseline["number_of_points"] = -1
+        baseline["intervals"] = [ \
+            [0.00014082795520386246, 0.00017553953889452146], \
+            [-13085083.850529518, 25085082.546604555], \
+            [-0.015084203011126875, 0.081675441022775491], \
+            [-81.474993833023916, 82.733480384279488]]
+        baseline["ellipsoid"] = [ \
+            [  1.15263027e-06, -2.05992587e+04,  1.63574438e-04, -3.81288306e-01], \
+            [ -2.05992587e+04,  1.39376409e+18,  1.11852245e+08, -1.51503797e+12], \
+            [  1.63574438e-04,  1.11852245e+08,  8.95632179e+00, -1.19176214e+04], \
+            [ -3.81288306e-01, -1.51503797e+12, -1.19176214e+04,  2.57948078e+07]]
+        baseline["delta"] = [ \
+            [  0.00000001e-06,  0.00000001e+04,  0.00000001e-04,  0.00000001e-01], \
+            [  0.00000001e+04,  0.00000001e+18,  0.00000001e+08,  0.00000001e+12], \
+            [  0.00000001e-04,  0.00000001e+08,  0.00000001e+00,  0.00000001e+04], \
+            [  0.00000001e-01,  0.00000001e+12,  0.00000001e+04,  0.00000001e+07]]
         baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "LCR benchmark model"
-        self.do_test_compute_linearised_confidence_region(self.do_experiment_setup_with_low_confidence(), None)
-        self.assertFalse(True)
+        baseline["plotdata"]["window_title"] = "LCR benchmark model (25%)"
+        self.do_test_compute_linearised_confidence_region(self.do_experiment_setup_with_low_confidence(), baseline)
 
 
 if __name__ == "__main__":
