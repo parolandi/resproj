0a3783e01f88c212ac160f6a80bfeaa046bd833a
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-05-12 05:27:47 +0100

    Test. Extend conditions tested.

--- experiments/experiment_11.py
+++ experiments/experiment_11.py
@@ -2,10 +2,10 @@
 import unittest
 import setups.kremlingetal_bioreactor as skb
 
-import numpy
+import logging
 import time
 
-import common.diagnostics as cd
+import common.diagnostics as codi
 import common.utilities as cu
 import data.generator as dg
 import results.plot_tiles as rpt
@@ -19,24 +19,20 @@ Kremling bioreactor
 Prediction uncertainty
 Splicing at 111000
 Covariance trace ~10%
+Both 0-20 and 0-60
 '''
 class TestExperiment11(unittest.TestCase):
 
 
     def __init__(self, *args, **kwargs):
         super(TestExperiment11, self).__init__(*args, **kwargs)
+        logging.basicConfig(filename=codi.get_name_logging_file(),level=codi.get_logging_level())
         self.do_plotting = True
 
     
-    def do_experiment_setup(self):
-        return skb.do_experiment_setup_0_60()
-
-
     def do_algorithm_setup(self):
         algorithm = dict(smiv.montecarlo_multiple_simulation_params)
         nominals = [7.23232059e-05, 6.00000000e+06, 1.67959956e-02, 1.00866368e-02]
-        # nominal confidence intervals
-        _ = [7.55459755277e-08, 9.72361400229e+16, 4.25111892419, 4768.80323967]
         conf_intvs = [7.55459755277e-08 / nominals[0], 0.5, 0.5, 0.5]
         algorithm["decision_variable_ranges"] = []
         for ii in range(len(nominals)):
@@ -47,21 +43,21 @@ class TestExperiment11(unittest.TestCase):
         return algorithm
 
     
-    def test_prediction_uncertainty(self):
+    def do_test_prediction_uncertainty(self, config, baseline):
         no_volume = 1
 
-        model, data, problem, _ = sesedaut.get_model_data_problem_algorithm(self.do_experiment_setup())
+        model, data, problem, _ = sesedaut.get_model_data_problem_algorithm(config)
         algorithm = self.do_algorithm_setup()
 
         wall_time0 = time.time()
         result = smiv.solve(model, problem, algorithm)
         wall_time = time.time() - wall_time0
-        cd.print_wall_time_message(wall_time)
+        codi.print_wall_time_message(wall_time)
         ensembles = result["succeeded"]["trajectories"][:,no_volume:,:]
         
         actual = cu.get_maximum_absolute_ensemble_values(ensembles)
-        self.assertAlmostEqual(actual[0,2], 0.44510079, 8)
-        self.assertAlmostEqual(actual[99,30], 1.50490147, 8)
+        self.assertAlmostEqual(actual[0,2], baseline[0], 8)
+        self.assertAlmostEqual(actual[99,-1], baseline[1], 8)
 
         if self.do_plotting:     
             plot_config = {}
@@ -70,6 +66,20 @@ class TestExperiment11(unittest.TestCase):
             rpt.plot_ensemble_trajectories(problem["time"], ensembles, problem["outputs"], errors, plot_config)
             rpl.plot_histogram_cutoff_by_count(result["succeeded"]["objective_function"], 20, 3)
             rpt.show_all()
+
+
+    def test_prediction_uncertainty_0_60(self):
+        baseline = []
+        baseline.append(0.44510079)
+        baseline.append(1.50490147)
+        self.do_test_prediction_uncertainty(skb.do_experiment_setup_0_60(), baseline)
+
+
+    def test_prediction_uncertainty_0_20(self):
+        baseline = []
+        baseline.append(0.44510079)
+        baseline.append(0.57332800)
+        self.do_test_prediction_uncertainty(skb.do_experiment_setup_0_20(), baseline)
 
 
 if __name__ == "__main__":
