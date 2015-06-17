3c841f94cffa80b0de10383671928de04f0a7535
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-06-17 21:45:55 +0100

    Style.

--- test/common/diagnostics.py
+++ test/common/diagnostics.py
@@ -8,6 +8,7 @@ class TestDiagnostics(unittest.TestCase):
 
     def test_get_date_and_time(self):
         print(testme.get_date_and_time())
+        self.assertTrue(True)
 
 
 if __name__ == "__main__":
