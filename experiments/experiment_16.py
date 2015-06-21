209faec1d2d9145b3a1a3b27ed2c93dd0e492f76
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-06-21 21:01:03 +0100

    New. Perform calibration and validation.

--- experiments/experiment_16.py
+++ experiments/experiment_16.py
@@ -6,7 +6,6 @@ import logging
 import numpy
 
 import common.diagnostics as codi
-import results.plot_data as replda
 import workflows.experiments as woex
 import workflows.reporting_unlegacy as wore
 
@@ -46,21 +45,23 @@ class TestExperiment16(unittest.TestCase):
             wore.plot_tiled_calibration_and_validation_trajectories_at_point(experiment(), calibrated)
 
 
-    def dn_test_nonlinear_confidence_region(self):
-        if self.do_quick_tests_only:
-            return
-        baseline = {}
-        baseline["number_of_points"] = 1
-        baseline["intervals"] = [ \
-            [4.721085304328252e-05, 0.00014639408975853561], \
-            [5999537.6911295187, 10015366.695155494], \
-            [0.0030218129834986489, 1.742703135385864], \
-            [0.00041379366926754218, 2.0432561489154017]]
-        baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "NCR benchmark model (95%)"
-        woex.test_calibration_with_nonlinear_confidence_region( \
-            sekrbi.do_experiment_protocol_setup_0_60_calib_ncr(), baseline, self)
+    def test_calibration_and_validation(self):
+        baseline = dict(woex.calib_valid_baseline)
+        basepoint = baseline["calib"]
+        basepoint["point"]["objective_function"] = 91.42486076854522
+        basepoint["point"]["decision_variables"] = numpy.array( \
+            [  7.09002587e-05,   6.01415123e+06,   7.70693208e-03, 1.85838333e-01])
+        basepoint["of_delta"] = 0.000000001
+        basepoint["dv_deltas"] = numpy.array( \
+            [  0.00000001e-05,  0.00000001e+06,  0.00000001e-03,  0.00000001e-01])
+        basepoint = baseline["valid"]
+        basepoint["point"]["objective_function"] = 181.34162033753552
+        
+        experiment = sekrbi.do_experiment_setup_0_60_spliced_yesyesno
+        calibrated = woex.test_baseline_calibration_and_validation(experiment, baseline, self)
+        if self.do_plotting:
+            wore.plot_tiled_calibration_and_validation_trajectories_at_point(experiment(), calibrated)
 
-
+    
 if __name__ == "__main__":
     unittest.main()
