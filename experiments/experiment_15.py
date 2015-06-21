bfbf4a0617b68f50b92f93e4ea3b14efca18ae2a
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-06-21 21:00:19 +0100

    Cleanup. Remove dead code.

--- experiments/experiment_15.py
+++ experiments/experiment_15.py
@@ -6,7 +6,6 @@ import logging
 import numpy
 
 import common.diagnostics as codi
-import results.plot_data as replda
 import workflows.experiments as woex
 import workflows.reporting as wore
 
@@ -44,22 +43,6 @@ class TestExperiment15(unittest.TestCase):
         calibrated = woex.test_baseline_calibration(experiment, basepoint, self)
         if self.do_plotting:
             wore.plot_tiled_trajectories_at_point(experiment(), calibrated)
-
-
-    def dn_test_nonlinear_confidence_region(self):
-        if self.do_quick_tests_only:
-            return
-        baseline = {}
-        baseline["number_of_points"] = 1
-        baseline["intervals"] = [ \
-            [4.721085304328252e-05, 0.00014639408975853561], \
-            [5999537.6911295187, 10015366.695155494], \
-            [0.0030218129834986489, 1.742703135385864], \
-            [0.00041379366926754218, 2.0432561489154017]]
-        baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "NCR benchmark model (95%)"
-        woex.test_calibration_with_nonlinear_confidence_region( \
-            sekrbi.do_experiment_protocol_setup_0_60_calib_ncr(), baseline, self)
 
 
 if __name__ == "__main__":
