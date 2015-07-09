94f5f16ad857d235d43ce8fffccecac490e71e89
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-07-09 21:24:32 +0100

    Results. New; 3d combinatorial plotting. Unit test.

--- test/results/plot_3d.py
+++ test/results/plot_3d.py
@@ -13,6 +13,12 @@ class TestPlot3d(unittest.TestCase):
         testme.plot_3d(region, [1,2,3])
         self.assertTrue(True)
 
+    
+    def test_plot_3d_combinatorial_from_file(self):
+        region = np.loadtxt("C:/workspace/resproj/test/mock/multiplepoints.txt",delimiter=',')
+        testme.plot_3d_combinatorial(region)
+        self.assertTrue(True)
+
 
 if __name__ == "__main__":
     unittest.main()
