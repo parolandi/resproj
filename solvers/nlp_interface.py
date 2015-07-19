d8c98738ad033e906f9930f66cec2c872a73ca59
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-07-19 15:39:24 +0100

    Fix. Handle failure in monte-carlo multiple.

--- solvers/nlp_interface.py
+++ solvers/nlp_interface.py
@@ -3,6 +3,8 @@ import models.model_data as momoda
 import solvers.least_squares as solesq
 import solvers.monte_carlo_multiple_least_squares as somcmlesq
 
+import logging
+
 
 def solve(model, problem, algorithm):
     """
@@ -10,17 +12,21 @@ def solve(model, problem, algorithm):
     algorithm: solver settings
     returns: models.model_data.NumericResult
     """
+    logging.debug("solvers.nlp_interface")
     if algorithm["class"] == solesq.solve:
         point = solesq.solve(model, problem, algorithm)
     elif algorithm["class"] == somcmlesq.solve:
         result = somcmlesq.solve(model, problem, algorithm)
         data = {}
         data["status"] = 0
+        data["x"] = result["decision_variables"]
         # TEST: 2015-06-23
         if len(result["decision_variables"]) == 0:
             data["status"] = 1
-        data["x"] = result["decision_variables"]
+            # WIP 2015-07-12; this doesn't work
+            data["x"] = algorithm["initial_guesses"]
         point = momoda.NumericResult(data)
     else:
         raise
+    logging.info("NLP point: " + str(point))
     return point
