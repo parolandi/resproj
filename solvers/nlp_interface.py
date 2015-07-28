897afa7f21850ccdf29823f5ff889c720c07de2c
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-07-28 05:29:00 +0100

    Solvers. Add new unlegacy method to cope with evolving data structures.

--- solvers/nlp_interface.py
+++ solvers/nlp_interface.py
@@ -6,6 +6,32 @@ import solvers.monte_carlo_multiple_least_squares as somcmlesq
 import logging
 
 
+def solve_unlegacy(model, problem, algorithm):
+    """
+    Solve nonlinear programming problem for generic (local, global) solver settings
+    algorithm: solver settings
+    returns: models.model_data.NumericResult
+    """
+    logging.debug("solvers.nlp_interface")
+    if algorithm["call"] == solesq.solve:
+        point = solesq.solve(model, problem, algorithm)
+    elif algorithm["call"] == somcmlesq.solve:
+        result = somcmlesq.solve(model, problem, algorithm)
+        data = {}
+        data["status"] = 0
+        data["x"] = result["decision_variables"]
+        # TEST: 2015-06-23
+        if len(result["decision_variables"]) == 0:
+            data["status"] = 1
+            # WIP 2015-07-12; this doesn't work
+            data["x"] = algorithm["initial_guesses"]
+        point = momoda.NumericResult(data)
+    else:
+        raise
+    logging.info("NLP point: " + str(point))
+    return point
+
+
 def solve(model, problem, algorithm):
     """
     Solve nonlinear programming problem for generic (local, global) solver settings
