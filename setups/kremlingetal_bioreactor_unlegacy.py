e6dde715d7a292d916a7c7b47c6bbba0de6dfd17
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-07-29 06:29:43 +0100

    Setups. Style.

--- setups/kremlingetal_bioreactor_unlegacy.py
+++ setups/kremlingetal_bioreactor_unlegacy.py
@@ -9,14 +9,29 @@ import setups.setup_data as seseda
 import solvers.solver_utils as sosout
 
 # --------------------------------------------------------------------------- #
+# Algorithm setup
 
-# TODO: rename
-def do_algorithm_setup_modelcalib_nm(instrumentation_data):
+def do_algorithm_setup_default(instrumentation_data):
+    """
+    Settings as follows
+    solvers.model_calibration.least_squares.numerics        := lq.solve
+    solvers.model_calibration.least_squares.method          := nelder-mead
+    solvers.model_calibration.least_squares.initial_guesses := nominal
+    solvers.model_calibration.least_squares.call_back       := none
+    solvers.parameter_confidence_estimation.region_estimation.nonlinear_programming.numerics := nlpi.solve
+    solvers.parameter_confidence_estimation.region_estimation.nonlinear_programming.call     := nlpi.lq
+    solvers.parameter_confidence_estimation.region_estimation.nonlinear_programming.method   := 'slsqp'
+    solvers.parameter_confidence_estimation.region_estimation.nonlinear_programming.callback := dvs-positive-and-log
+    solvers.parameter_confidence_estimation.region_estimation.monte_carlo_simulation.numerics                 := mcmiv.solve
+    solvers.parameter_confidence_estimation.region_estimation.monte_carlo_simulation.number_of_trials         := 10
+    solvers.parameter_confidence_estimation.region_estimation.monte_carlo_simulation.decision_variable_ranges := 0.1,10
+    """
     algorithm_data = dict(alalda.algorithmic_data)
     algorithm_data["solvers"] = dict(alalda.solvers_data)
     
     algorithm_data["solvers"]["model_calibration"] = dict(alalda.numerics_data)
     ls = algorithm_data["solvers"]["model_calibration"]["least_squares"]
+
     ls["numerics"] = alalse.sonlpi.solesq.solve
     ls["method"] = 'Nelder-Mead'
     ls["initial_guesses"] = [7e-05, 6000000.0, 0.0168, 0.01]
@@ -38,12 +53,13 @@ def do_algorithm_setup_modelcalib_nm(instrumentation_data):
     return algorithm_data
 
 # --------------------------------------------------------------------------- #
+# Protocol setup
 
 def do_protocol_setup_0_20_default():
     protocol = copy.deepcopy(seseda.experiment_protocol)
     protocol["steps"] = []
     setup = sekrbi.do_experiment_setup_0_20()
-    setup["algorithm_setup"] = do_algorithm_setup_modelcalib_nm
+    setup["algorithm_setup"] = do_algorithm_setup_default
     setup["local_setup"]["do_plotting"] = False
     protocol["steps"].append(copy.deepcopy(setup))
     protocol["steps"].append(copy.deepcopy(setup))
@@ -62,7 +78,7 @@ def do_protocol_setup_0_20_2x_default():
     protocol = copy.deepcopy(seseda.experiment_protocol)
     protocol["steps"] = []
     setup = sekrbi.do_experiment_setup_0_20_twice()
-    setup["algorithm_setup"] = do_algorithm_setup_modelcalib_nm
+    setup["algorithm_setup"] = do_algorithm_setup_default
     setup["local_setup"]["do_plotting"] = False
     protocol["steps"].append(copy.deepcopy(setup))
     protocol["steps"].append(copy.deepcopy(setup))
@@ -73,7 +89,7 @@ def do_protocol_setup_0_60_default():
     protocol = copy.deepcopy(seseda.experiment_protocol)
     protocol["steps"] = []
     setup = sekrbi.do_experiment_setup_0_60()
-    setup["algorithm_setup"] = do_algorithm_setup_modelcalib_nm
+    setup["algorithm_setup"] = do_algorithm_setup_default
     setup["local_setup"]["do_plotting"] = False
     protocol["steps"].append(copy.deepcopy(setup))
     protocol["steps"].append(copy.deepcopy(setup))
