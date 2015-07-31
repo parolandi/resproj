b8b8604139ca29cc0dd6e1b03c294cb287068fed
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-07-31 06:53:59 +0100

    Solvers. Differentiate between positive and bounded decision variable enforcements.

--- solvers/solver_utils.py
+++ solvers/solver_utils.py
@@ -13,7 +13,24 @@ class DecisionVariableLogger():
 
 
     def let_decision_variables_be_positive_and_log(self, x):
-        logit = False
+        logit = True
+        
+        eps = 1e-30
+        corrected = []
+        for ii in range(len(x)):
+            if x[ii] < eps:
+                x[ii] = eps
+                corrected.append(ii)
+        if len(corrected) > 0 and logit:
+            print("corrected", x, corrected)
+
+        self.decision_variables.append([x])
+        if logit:
+            logging.info(x)
+        
+        
+    def let_decision_variables_be_bounded_and_log(self, x):
+        logit = True
         
         eps_lb = 1E-10
         eps_ub = 1E+10
@@ -32,8 +49,8 @@ class DecisionVariableLogger():
         self.decision_variables.append([x])
         if logit:
             logging.info(x)
-        
-        
+
+    
     def print_decision_variables(self):
         print("Decision variables", self.decision_variables)
 
