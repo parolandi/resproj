f2e96c2e32a6afa501b806a26c965e7669eb3a17
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-08-23 10:57:05 +0100

    Fix. Test was being done for yes-no-yes instead of yes-yes-no.

--- experiments/experiment_22.py
+++ experiments/experiment_22.py
@@ -7,7 +7,8 @@ import logging
 
 import common.diagnostics as codi
 import common.environment as coen
-import results.plot_data as replda
+#import results.plot_data as replda
+import results.plot_utils as replut
 import workflows.experiments as woex
 
 
@@ -39,26 +40,24 @@ class TestExperiment22(unittest.TestCase):
         logging.info(codi.get_date_and_time())
 
 
+    def get_baseline_nonlinear_confidence_region(self):
+        baseline = dict(woex.calib_valid_baseline)
+        baseline["plotdata"] = dict(replut.plda.plot_data)
+        basepoint = baseline["calib"]
+        basepoint = exba.set_baseline_point_0_60_yesyesno(basepoint)
+        basepoint = exba.set_baseline_eps_0_60_yesyesno(basepoint)
+        baseline = exba.set_baseline_nonlinconfreg_0_60_yesyesno(baseline)
+        baseline = replut.set_window_title(baseline, "Exp-22: NCR benchmark model (95%)")
+        return baseline
+
+
     def test_nonlinear_confidence_region(self):
         logging.debug("experiments.experiment_22.test_calibration_and_validation")
         if self.do_quick_tests_only:
             codi.print_and_log_return_on_quick_tests_only()
             return
-        baseline = dict(woex.calib_valid_baseline)
-        basepoint = baseline["calib"]
-        basepoint = exba.set_baseline_point_0_60_yesnoyes(basepoint)
-        basepoint = exba.set_baseline_eps_0_60_yesnoyes(basepoint)
-        baseline["number_of_points"] = 0
-        # 
-        baseline["intervals"] = [ \
-            [5.303839254978861e-05, 6.7928643137517991e-05], \
-            [5900150.185800408, 59065454.091839552], \
-            [0.01009237303638489, 0.01429891307608047], \
-            [0.018628141421802392, 0.033995558368361149]]
-        baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "Exp-22: NCR benchmark model (95%)"
-        
-        experiment = sekrbitoo.do_protocol_setup_0_60_yesnoyes
+        baseline = self.get_baseline_nonlinear_confidence_region()
+        experiment = sekrbitoo.do_protocol_setup_0_60_yesyesno
         woex.test_calibration_with_nonlinear_confidence_region(experiment(), baseline, self)
 
 
