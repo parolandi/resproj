ecc32bfa8c5a2bb8d5dcca977616787461216857
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-08-23 13:27:18 +0100

    Test. Add lin conf reg for experiment-23.

--- experiments/experiment_23.py
+++ experiments/experiment_23.py
@@ -1,5 +1,6 @@
 
 import unittest
+import setups.kremlingetal_bioreactor as sekrbi
 import setups.kremlingetal_bioreactor_unlegacy as sekrbitoo
 import experiments.baselines as exba
 
@@ -7,7 +8,7 @@ import logging
 
 import common.diagnostics as codi
 import common.environment as coen
-import results.plot_data as replda
+import results.plot_utils as replut
 import workflows.experiments as woex
 
 
@@ -39,27 +40,43 @@ class TestExperiment23(unittest.TestCase):
         logging.info(codi.get_date_and_time())
 
 
-    def test_nonlinear_confidence_region(self):
+    def get_baseline_nonlinear_confidence_region(self):
+        baseline = dict(woex.calib_valid_baseline)
+        baseline["plotdata"] = dict(replut.plda.plot_data)
+        basepoint = baseline["calib"]
+        basepoint = exba.set_baseline_point_0_60_yes10yes15no5(basepoint)
+        basepoint = exba.set_baseline_eps_0_60_yes10yes15no5(basepoint)
+        baseline = exba.set_baseline_nonlinconfreg_0_60_yes10yes15no5(baseline)
+        baseline = replut.set_window_title(baseline, "Exp-23: NCR benchmark model (95%)")
+        return baseline
+
+    
+    def did_test_nonlinear_confidence_region(self):
         logging.debug("experiments.experiment_23.test_calibration_and_validation")
         if self.do_quick_tests_only:
             codi.print_and_log_return_on_quick_tests_only()
             return
+        experiment = sekrbitoo.do_protocol_setup_0_60_yes10yes15no5
+        baseline = self.get_baseline_nonlinear_confidence_region()        
+        woex.test_calibration_with_nonlinear_confidence_region(experiment(), baseline, self)
+
+
+    def get_baseline_linearised_confidence_region(self):
         baseline = dict(woex.calib_valid_baseline)
+        baseline["plotdata"] = dict(replut.plda.plot_data)
         basepoint = baseline["calib"]
         basepoint = exba.set_baseline_point_0_60_yes10yes15no5(basepoint)
         basepoint = exba.set_baseline_eps_0_60_yes10yes15no5(basepoint)
-        baseline["number_of_points"] = 0
-        # [8, 9], [8, 9], [9, 4], [9, 9]
-        baseline["intervals"] = [ \
-            [8.3865274119544132e-05, 8.7361023095616056e-05], \
-            [5759553.1188991377, 5776681.8321100501], \
-            [0.0093196862993481545, 0.093196862993481538], \
-            [0.0043275124884777994, 0.43275124884777993]]
-        baseline["plotdata"] = dict(replda.plot_data)
-        baseline["plotdata"]["window_title"] = "Exp-23: NCR benchmark model (95%)"
-        
-        experiment = sekrbitoo.do_protocol_setup_0_60_yes10yes15no5
-        woex.test_calibration_with_nonlinear_confidence_region(experiment(), baseline, self)
+        baseline = exba.set_baseline_linconfreg_0_60_yes10yes15no5(baseline)
+        baseline = replut.set_window_title(baseline, "Exp-23: LCR benchmark model (95%)")
+        return baseline
+
+
+    def test_linearised_confidence_region(self):
+        logging.debug("experiments.experiment_23.test_linearised_confidence_region")
+        experiment = sekrbi.do_experiment_setup_0_60_spliced_yes10yes15no5
+        baseline = self.get_baseline_linearised_confidence_region()
+        woex.test_calibration_with_linearised_confidence_region(experiment(), baseline, self)
 
 
 if __name__ == "__main__":
