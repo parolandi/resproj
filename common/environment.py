bc4a3752b3127a48b45cace499f195de3efc0536
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2015-09-12 16:20:05 +0100

    Common. Constants for environment.

--- common/environment.py
+++ common/environment.py
@@ -5,3 +5,11 @@ def get_doing_quick_tests_only():
 
 def get_doing_plotting():
     return False
+
+
+def get_results_location():
+    return "C:/Users/mamuts/Desktop/"
+
+
+def get_objective_function_and_decision_variables_file_name():
+    return "hpnts.csv"
