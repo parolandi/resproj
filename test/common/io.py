d25a35db3d6e775cfdb5a34a2c0e0927594d4b88
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2016-04-19 21:16:03 -0400

    Migration. Package test/common.

--- test/common/io.py
+++ test/common/io.py
@@ -3,6 +3,7 @@ import unittest
 import common.io as testme
 
 import numpy as np
+import common.environment as coen
 
 
 class TestIO(unittest.TestCase):
@@ -10,18 +11,18 @@ class TestIO(unittest.TestCase):
 
     def test_write_to_csv(self):
         vals = np.ones((4,5))
-        testme.write_to_csv(vals, "C:/workspace/resproj/test/common/test_write_to_csv.csv")
+        testme.write_to_csv(vals, coen.get_src_location() + "/test/common/test_write_to_csv.csv")
         self.assertTrue(True)
         
         
     def test_read_from_csv(self):
-        testme.read_from_csv("C:/workspace/resproj/test/common/test_read_from_csv.csv")
+        testme.read_from_csv(coen.get_src_location() + "/test/common/test_read_from_csv.csv")
         self.assertTrue(True)
 
 
     def test_write_to_txt(self):
         vals = np.ones((4,5))
-        testme.write_to_txt(np.array_str(vals), "C:/workspace/resproj/test/common/test_write_to_txt.txt")
+        testme.write_to_txt(np.array_str(vals), coen.get_src_location() + "/test/common/test_write_to_txt.txt")
         self.assertTrue(True)
 
 
