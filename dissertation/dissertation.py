93caebfbe111826ef1bc78243128368c5fba7fe6
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2016-05-08 06:22:36 -0400

    Dissertation. Add figure 01.

---
+++ dissertation/dissertation.py
@@ -0,0 +1,51 @@
+
+import common.environment as coen
+import results.plot_data as replda
+import results.plot_records as replre
+
+class Figure01():
+    def get_plot_config(self):
+        count = 5
+        config = replda.MultiPlotFormattingData(count).set_layout_data( \
+            replda.MultiPlotLayoutData().set_no_cols_no_rows_indices(1, count, [0,1]))
+        config.set_formatting_data( \
+            replda.SinglePlotFormattingData(). \
+                set_x_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"Time")). \
+                set_y_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"X")). \
+                set_trace(replda.PlotTraceFormattingData().set_colour_mark(replda.get_plot_colours(6)[0], 'o')), \
+                0)
+        config.set_formatting_data( \
+            replda.SinglePlotFormattingData(). \
+                set_x_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"Time")). \
+                set_y_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"S")). \
+                set_trace(replda.PlotTraceFormattingData().set_colour_mark(replda.get_plot_colours(6)[1], 'o')), \
+                1)
+        config.set_formatting_data( \
+            replda.SinglePlotFormattingData(). \
+                set_x_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"Time")). \
+                set_y_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"M1")). \
+                set_trace(replda.PlotTraceFormattingData().set_colour_mark(replda.get_plot_colours(6)[2], 'o')), \
+                2)
+        config.set_formatting_data( \
+            replda.SinglePlotFormattingData(). \
+                set_x_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"Time")). \
+                set_y_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"M2")). \
+                set_trace(replda.PlotTraceFormattingData().set_colour_mark(replda.get_plot_colours(6)[3], 'o')), \
+                3)
+        config.set_formatting_data( \
+            replda.SinglePlotFormattingData(). \
+                set_x_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"Time")). \
+                set_y_axis(replda.PlotAxisFormattingData().set_min_max_label(0,0,"E")). \
+                set_trace(replda.PlotTraceFormattingData().set_colour_mark(replda.get_plot_colours(6)[4], 'o')), \
+                4)
+        return config
+
+    def plot_it(self):
+        file_meas = coen.get_results_location()+"calib_measured.csv"
+        file_pred = coen.get_results_location()+"calib_predicted.csv"
+        config = self.get_plot_config()
+        replre.plot_tiled_calibration_and_validation_trajectories_at_record( \
+            file_meas, file_pred, config)
+
+if __name__ == '__main__':
+    Figure01().plot_it()
