05f6959bc9c172432427e7cc9f110ab30581bcb6
Pablo A Rolandi <p.a.rolandi.aux@gmail.com>
2016-05-22 07:14:09 -0400

    Results. Format 3D plots. Test. Add unit test.

--- results/plot_3d.py
+++ results/plot_3d.py
@@ -11,58 +11,61 @@ def plot_3d(data, indices):
                     np.transpose(data)[indices[1]], \
                     np.transpose(data)[indices[2]])
     pp.show()
-   
 
-def plot_3d_combinatorial(data):
-    '''
-    data    numpy.array
-    '''
-    #no_grid = 4
-    fig = pp.figure("NCR projections")
-    sp = fig.add_subplot(2, 2, 1, projection='3d')
-    indices = [0,1,2]
-    sp.scatter(np.transpose(data)[indices[0]], \
-                    np.transpose(data)[indices[1]], \
-                    np.transpose(data)[indices[2]])
-    sp = fig.add_subplot(2, 2, 2, projection='3d')
-    indices = [0,1,3]
-    sp.scatter(np.transpose(data)[indices[0]], \
-                    np.transpose(data)[indices[1]], \
-                    np.transpose(data)[indices[2]])
-    sp = fig.add_subplot(2, 2, 3, projection='3d')
-    indices = [0,2,3]
-    sp.scatter(np.transpose(data)[indices[0]], \
-                    np.transpose(data)[indices[1]], \
-                    np.transpose(data)[indices[2]])
-    sp = fig.add_subplot(2, 2, 4, projection='3d')
-    indices = [1,2,3]
-    sp.scatter(np.transpose(data)[indices[0]], \
-                    np.transpose(data)[indices[1]], \
-                    np.transpose(data)[indices[2]])
-    pp.show()
 
+def set_labels(config, plot, indices):
+    if config is None:
+        return
+    plot.set_xlabel(config.axes[indices[0]].label)
+    plot.set_ylabel(config.axes[indices[1]].label)
+    plot.set_zlabel(config.axes[indices[2]].label)
+    
 
 def plot_nonlinear_confidence_region_3D_projections_combinatorial(config, region):
+    """
+    Shows at the end
+    config  TODOC
+    data    numpy.array
+    """
+    # HACK
     data = region[0:1000]
-    fig = pp.figure("NCR projections")
+    fig = pp.figure("3D NCR projections")
+    
     sp = fig.add_subplot(2, 2, 1, projection='3d')
     indices = [0,1,2]
     sp.scatter(np.transpose(data)[indices[0]], \
                     np.transpose(data)[indices[1]], \
                     np.transpose(data)[indices[2]])
+    set_labels(config, sp, indices)
+    
     sp = fig.add_subplot(2, 2, 2, projection='3d')
     indices = [0,1,3]
     sp.scatter(np.transpose(data)[indices[0]], \
                     np.transpose(data)[indices[1]], \
                     np.transpose(data)[indices[2]])
+    set_labels(config, sp, indices)
+    
     sp = fig.add_subplot(2, 2, 3, projection='3d')
     indices = [0,2,3]
     sp.scatter(np.transpose(data)[indices[0]], \
                     np.transpose(data)[indices[1]], \
                     np.transpose(data)[indices[2]])
+    set_labels(config, sp, indices)
+    
     sp = fig.add_subplot(2, 2, 4, projection='3d')
     indices = [1,2,3]
     sp.scatter(np.transpose(data)[indices[0]], \
                     np.transpose(data)[indices[1]], \
                     np.transpose(data)[indices[2]])
+    set_labels(config, sp, indices)
+    
     pp.show()
+    
+    
+def plot_3d_combinatorial(data):
+    '''
+    Legacy. Use:
+    plot_nonlinear_confidence_region_3D_projections_combinatorial
+    data    numpy.array
+    '''
+    plot_nonlinear_confidence_region_3D_projections_combinatorial(None, data)
--- test/results/plot_3d.py
+++ test/results/plot_3d.py
@@ -4,19 +4,41 @@ import results.plot_3d as testme
 
 import numpy as np
 
+import common.environment as coen
+import results.plot_data as replda
+
 
 class TestPlot3d(unittest.TestCase):
 
+    def get_plot_config(self):
+        config = replda.TiledPlotFormattingData(4)
+        config.set_axes_data( \
+            replda.PlotAxisFormattingData().set_min_max_label(0,0,"p1"), 0). \
+        set_axes_data(
+            replda.PlotAxisFormattingData().set_min_max_label(0,0,"p2"), 1). \
+        set_axes_data(
+            replda.PlotAxisFormattingData().set_min_max_label(0,0,"p3"), 2). \
+        set_axes_data(
+            replda.PlotAxisFormattingData().set_min_max_label(0,0,"p4"), 3)
+        return config
+    
 
-    def test_plot_3d_from_file(self):
-        region = np.loadtxt("C:/workspace/resproj/test/mock/multiplepoints.txt",delimiter=',')
+    def dn_test_plot_3d_from_file(self):
+        region = np.loadtxt(coen.get_test_mock_location() + "/multiplepoints.txt", delimiter=',')
         testme.plot_3d(region, [1,2,3])
         self.assertTrue(True)
 
     
-    def test_plot_3d_combinatorial_from_file(self):
-        region = np.loadtxt("C:/workspace/resproj/test/mock/multiplepoints.txt",delimiter=',')
+    def dn_test_plot_3d_combinatorial_from_file(self):
+        region = np.loadtxt(coen.get_test_mock_location() + "/multiplepoints.txt", delimiter=',')
         testme.plot_3d_combinatorial(region)
+        self.assertTrue(True)
+
+
+    def test_plot_nonlinear_confidence_region_3D_projections_combinatorial(self):
+        config = self.get_plot_config()
+        region = np.loadtxt(coen.get_test_mock_location() + "/multiplepoints.txt", delimiter=',')
+        testme.plot_nonlinear_confidence_region_3D_projections_combinatorial(config, region)
         self.assertTrue(True)
 
 
